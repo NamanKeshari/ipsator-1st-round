bd19cdc2394afdd86dba44b9498e2004c3ff5a7f
NamanKeshari <72098614+NamanKeshari@users.noreply.github.com>
2023-08-02 20:09:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
+import { useEffect, useLayoutEffect, useState } from "react";
 import "./styles.css";
-import { useQuery } from "./useQuery";
+// import { useQuery } from "./useQuery";
 
 export default function App() {
-  const { data } = useQuery({
-    url: "https://jsonplaceholder.typicode.com/posts/"
-  });
+  const [json, setjson] = useState({});
+  // const { data } = useQuery({
+  //   url: "https://jsonplaceholder.typicode.com/posts/"
+  // });
 
+  useLayoutEffect(() => {
+    console.log("hi i am naman");
+  }, []);
+
+  useEffect(() => {
+    setjson({ a: true, out: "a" });
+    setjson({ b: true, out: "b" });
+    setjson({ c: true, out: "c" });
+    setjson({ d: true, out: "d" });
+  }, []);
+
+  useEffect(() => {
+    console.log(json, "inside");
+  }, [json]);
+
+  console.log(json, "outside");
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      {typeof data === "string" ? (
+      <button
+        type="button"
+        onClick={(e) => {
+          setjson({ click: true });
+        }}
+      >
+        "Click Me"
+      </button>
+      {/* {typeof data === "string" ? (
         <div>{data}</div>
       ) : (
         data?.map((d, i) => (
@@ -18,7 +44,7 @@ export default function App() {
             {d?.id} {d?.title}
           </div>
         ))
-      )}
+      )} */}
     </div>
   );
 }
